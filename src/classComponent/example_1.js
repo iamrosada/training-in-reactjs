d540ab88d8ae6914367542864df6a98109d686b2
ÁGUA-ROSADA <59142372+77-aguarosada@users.noreply.github.com>
2021-05-01 03:58:16 -0300

    working with class and props,  the last commits i jsut updated my files

---
+++ src/classComponent/example_1.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class Equipe extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default function InClass() {
+  return (
+    <div>
+      <h1>Testando</h1>
+      <Equipe />
+    </div>
+  );
+}
