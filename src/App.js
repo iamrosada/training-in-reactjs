87a561fa98a67e4558dbcc1d801a67d33006c8ec
ÁGUA-ROSADA <59142372+77-aguarosada@users.noreply.github.com>
2021-05-01 04:26:17 -0300

    e

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
 
 /* import SecondProps from "./propsInFunction/example_2"; */
-import InClass from "./propsInClass/example_1";
+/* import InClass from './propsInClass/example_2'; */
+import Example from "./stateInClass/example_1";
 export default function App() {
   return (
     <div className="App">
-      <InClass />
+      <Example />
     </div>
   );
 }
