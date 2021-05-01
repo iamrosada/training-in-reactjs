3b21d856b988e708195510421fe961c889bbd692
ÁGUA-ROSADA <59142372+77-aguarosada@users.noreply.github.com>
2021-05-01 02:29:22 -0300

    first example in this example i showed how work props

--- src/props/propsInReact.js
+++ src/props/propsInReact.js
@@ -1,7 +1,18 @@
+const Student = (props) => {
+  return (
+    <div>
+      <h2>
+        bem vindo {props.name}, voce tem {props.idade} anos de idade
+      </h2>
+    </div>
+  );
+};
+
 function PropsInReact() {
   return (
     <div>
-      <h1> Studing Props in React </h1>
+      <Student name="luis" idade="22" />
+      <Student name="rosada" idade="21" />
     </div>
   );
 }
