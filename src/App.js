f6ab005219534e8941e2b560c1b3d08c9a9f42e9
ÁGUA-ROSADA <59142372+77-aguarosada@users.noreply.github.com>
2021-05-01 13:56:31 -0300

    working with class and state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./styles.css";
 
-/* import SecondProps from "./propsInFunction/example_2"; */
-/* import InClass from './propsInClass/example_2'; */
-import Example from "./stateInClass/example_1";
+import Example from "./stateClassinComp/example";
 export default function App() {
   return (
     <div className="App">
---
+++ src/stateClassinComp/example.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+class Equipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: "Luis",
+      contador: 0
+    };
+    this.aumentar = this.aumentar.bind(this);
+    this.deminuir = this.deminuir.bind(this);
+  }
+  aumentar() {
+    //o state esta a receber todos os estdos
+    let state = this.state;
+    state.contador += 1;
+    state.nome = "Rosada";
+    this.setState(state);
+  }
+  deminuir() {
+    let state_para_diminuir = this.state;
+    state_para_diminuir.contador -= 1;
+    //para nao ser negativo
+    if (state_para_diminuir.contador < 0) {
+      state_para_diminuir.contador = 0;
+    }
+    this.setState(state_para_diminuir);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        {this.state.nome}
+        <button onClick={this.aumentar}>+</button>
+        <h3>{this.state.contador}</h3>
+        <button onClick={this.deminuir}>-</button>
+      </div>
+    );
+  }
+}
+
+export default Equipe;
