f4dd198a18a850016812169336b2435f60e8e33a
ÁGUA-ROSADA <59142372+77-aguarosada@users.noreply.github.com>
2021-05-03 11:32:20 -0300

    example finish

--- src/conditional/example_2.js
+++ src/conditional/example_2.js
@@ -3,11 +3,26 @@ import { Component } from "react";
 class Example extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: false
+    };
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
-
+  login() {
+    this.setState({ status: true });
+  }
+  logout() {
+    this.setState({ status: false });
+  }
   render() {
-    return <div>fvff</div>;
+    return (
+      <div>
+        {this.state.status ? <h1> esta logado</h1> : <h1>Esta deslogado</h1>}
+        <button onClick={this.login}>Fazer Login</button>
+        <button onClick={this.logout}>Fazer Logout</button>
+      </div>
+    );
   }
 }
 
