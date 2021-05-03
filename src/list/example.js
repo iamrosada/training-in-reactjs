9c95c88106b8a3cda9ccb52129bf802d7b263b24
ÁGUA-ROSADA <59142372+77-aguarosada@users.noreply.github.com>
2021-05-03 12:19:48 -0300

    this is firts form

--- src/list/example.js
+++ src/list/example.js
@@ -1,12 +1,34 @@
 import { Component } from "react";
-
+import "./example.css";
 class Example extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      feed: [
+        { id: 1, username: "Luis", curtdas: 10, comentarios: 2 },
+        { id: 2, username: "Rosada", curtdas: 12, comentarios: 5 },
+        { id: 3, username: "Agua", curtdas: 212, comentarios: 125 }
+      ]
+    };
   }
   render() {
-    return <div>TESSTING</div>;
+    return (
+      <div>
+        {this.state.feed.map((item) => {
+          return (
+            <div id="card" key={item.id}>
+              <h2>{item.username}</h2>
+              <div id="reactions">
+                curtidas
+                <h3>{item.curtdas}</h3>
+                <h3>{item.comentarios}</h3>
+                comentarios
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
