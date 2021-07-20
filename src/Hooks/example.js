9a17037ab6b7691c109b88f467f3880b69d9309f
LUÍS DE ÁGUA-ROSADA <59142372+iamrosada@users.noreply.github.com>
2021-07-20 12:58:10 +0300

    using useState

--- src/Hooks/example.js
+++ src/Hooks/example.js
@@ -1,5 +1,26 @@
 import React, { useState } from "react";
 
 export default function Hookd() {
-  return <h2>HOOKS</h2>;
+  const [tarefas, setTarefas] = useState([
+    "Arumar a casa",
+    "Ir ao duch ",
+    "Comer Pão"
+  ]);
+
+  function handleTarefas() {
+    setTarefas([...tarefas, "Revisar o codigo"]);
+  }
+  return (
+    <div>
+      <h2>Minhas Tarefas</h2>
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleTarefas}>
+        Adicionar Tarefas
+      </button>
+    </div>
+  );
 }
